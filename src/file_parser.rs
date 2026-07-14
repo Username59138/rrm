173b5c699bfe4cf06051638d5b8e93ce04e7e6f5
username59138 <pukp27803@gmail.com>
2026-07-14 17:07:15 +0300

    fixed absolute path bug

--- src/file_parser.rs
+++ src/file_parser.rs
@@ -2,35 +2,32 @@ use crate::config_file::ConfigFile;
 use crate::input::get_yes_no;
 use crate::launch_config::LaunchConfig;
 use std::env;
-use std::path::PathBuf;
+use std::error::Error;
+use std::path::{PathBuf, absolute};
+
+fn return_launch_config_files(files: Option<&str>) -> Vec<PathBuf> {
+    files
+        .unwrap_or_default()
+        .split(',')
+        .map(|file| PathBuf::from(file))
+        .collect()
+}
 
 pub fn check_file(
     config_file: &ConfigFile,
     launch_config: &LaunchConfig,
     file: &PathBuf,
-) -> Option<PathBuf> {
-    let mut file = file.clone();
-    if !file.starts_with("/") {
-        file.push(env::current_dir().unwrap_or_else(|_| PathBuf::from("/")));
-    }
-    let include_files: Vec<&str> = launch_config
-        .include
-        .as_deref()
-        .unwrap_or_default()
-        .split(',')
-        .collect();
-    let exclude_files: Vec<&str> = launch_config
-        .exclude
-        .as_deref()
-        .unwrap_or_default()
-        .split(',')
-        .collect();
-    let confirm_files_arguments: Vec<&str> = launch_config
-        .confirm_files
-        .as_deref()
-        .unwrap_or_default()
-        .split(',')
-        .collect();
+) -> Result<Option<PathBuf>, Box<dyn Error>> {
+    let root_dir = env::current_dir()
+        .ok()
+        .and_then(|path| path.ancestors().last().map(PathBuf::from))
+        .unwrap_or_else(|| PathBuf::from("/"));
+
+    let file = absolute(file.clone())?;
+    let include_files = return_launch_config_files(launch_config.include.as_deref());
+    let exclude_files = return_launch_config_files(launch_config.exclude.as_deref());
+    let confirm_files_arguments =
+        return_launch_config_files(launch_config.confirm_files.as_deref());
 
     let (blacklist_files, very_blacklist_files, confirm_list_files) =
         if let Some(lists) = &config_file.lists {
@@ -50,49 +47,43 @@ pub fn check_file(
     if match confirm_list_files {
         Some(lists) => lists.contains(&file),
         _ => false,
-    } || confirm_files_arguments.contains(&file.to_str().unwrap_or_default())
+    } || confirm_files_arguments.contains(&file)
     {
         println!(
             "Do you want to add {} to delete list? [Y/n]",
             file.to_str().unwrap_or_default()
         );
-        let Ok(user_input) = get_yes_no() else {
-            return None;
-        };
+        let user_input = get_yes_no()?;
         if !user_input {
-            return None;
+            return Ok(None);
         }
     }
 
     if let Some(blacklist_files) = &blacklist_files
         && blacklist_files.contains(&file)
-        && !include_files.contains(&file.to_str().unwrap_or_default())
+        && !include_files.contains(&file)
     {
-        return None;
+        return Ok(None);
     }
     if let Some(very_blacklist_files) = &very_blacklist_files
         && very_blacklist_files.contains(&file)
     {
-        return None;
+        return Ok(None);
     }
-    let root_dir = env::current_dir()
-        .ok()
-        .and_then(|path| path.ancestors().last().map(PathBuf::from))
-        .unwrap_or_else(|| PathBuf::from("/"));
     if let Some(allow_root_deletion) = allow_root_deletion
         && !allow_root_deletion
         && (file == root_dir || file.parent() == Some(&root_dir))
     {
-        return None;
+        return Ok(None);
     } else if file == root_dir || file.parent() == Some(&root_dir) {
-        return None;
+        return Ok(None);
     };
-    if exclude_files.contains(&file.to_str().unwrap_or_default()) {
-        return None;
+    if exclude_files.contains(&file) {
+        return Ok(None);
     }
 
     if launch_config.verbose {
         println!("{} will be deleted", file.to_str().unwrap_or_default());
     }
-    Some(file)
+    Ok(Some(file))
 }
